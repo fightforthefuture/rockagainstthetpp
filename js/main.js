013b4ec7c4d57c9f8b7e1443ca795c7a0b837b0a
Aki Rose Braun <hi@akiro.se>
2016-05-31 21:01:53 -0700

    What up TPP siittteeeee

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 (function (doc, win) {
-    "use strict";
+  "use strict";
 
+  var
+    countryLabel = doc.querySelector('[for="select-country"]'),
+    countrySelect = doc.getElementById('select-country');
+
+  countryLabel.addEventListener('click', function (e) {
+    countryLabel.classList.add('hidden');
+    countrySelect.classList.remove('hidden');
+    countrySelect.setAttribute('name', 'signature[country]');
+  })
 })(document, window);
