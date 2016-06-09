d0b05ccc6307dd02f1316b7d31d62cbcb79adf87
Aki Rose Braun <hi@akiro.se>
2016-06-08 22:07:05 -0600

    Adds share buttons

--- js/main.js
+++ js/main.js
@@ -3,11 +3,26 @@
 
   var
     countryLabel = doc.querySelector('[for="select-country"]'),
-    countrySelect = doc.getElementById('select-country');
+    countrySelect = doc.getElementById('select-country'),
+    tweetButton = doc.getElementById('tweet-button'),
+    shareButton = doc.getElementById('share-button');
 
   countryLabel.addEventListener('click', function (e) {
     countryLabel.classList.add('hidden');
     countrySelect.classList.remove('hidden');
     countrySelect.setAttribute('name', 'signature[country]');
-  })
+  });
+
+  tweetButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    win.open(tweetButton.getAttribute('href'), 'Tweet this', 'width=550,height=420,resizable=1')
+  });
+
+  shareButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    win.open(shareButton.getAttribute('href'), 'Share this', 'width=581,height=614,resizable=1')
+  });
+
 })(document, window);
