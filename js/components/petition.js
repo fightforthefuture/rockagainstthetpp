5a2fe8e44da5e2e195eef7dfec611717c8c9a369
DominoLink <lilia@dominolink.com>
2016-09-07 09:34:49 -0700

    boston ticket in success modal for dl

--- js/components/petition.js
+++ js/components/petition.js
@@ -95,6 +95,11 @@
     shareTicket.innerHTML = '<h4>1. Dont forget to grab your ticket:</h4><a href="http://bit.ly/2bnSehk" target="_blank"><button class="download-ticket">Download Ticket!</button></a><br><span style="font-size: 1.5rem;">(We also sent a copy to your email.)</span><br>';
     shareCopy.textContent = '2. Can you help spread the word?';
 
+     } else if (window.location.href.indexOf("boston") > -1) {
+    shareHeadline.textContent = "You're all set!";
+    shareTicket.innerHTML = '<h4>1. Dont forget to grab your ticket:</h4><a href="http://bit.ly/2cBAcpW" target="_blank"><button class="download-ticket">Download Ticket!</button></a><br><span style="font-size: 1.5rem;">(We also sent a copy to your email.)</span><br>';
+    shareCopy.textContent = '2. Can you help spread the word?';
+
      } else { 
     shareHeadline.textContent = "You're all set! We sent you an email.";  
     shareCopy.textContent = 'Now can you help spread the word?';
